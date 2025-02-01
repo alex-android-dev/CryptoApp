e7d7e8cdd52060501cefab1c45a73d51cf3bdf67
avlnkov <alex.lenkov@mail.ru>
2025-02-01 17:55:57 +0300

    l1: created domain layer

---
+++ app/src/main/java/com/example/cryptoapp/domain/GetCoinInfoUseCase.kt
@@ -0,0 +1,9 @@
+package com.example.cryptoapp.domain
+
+import androidx.lifecycle.LiveData
+
+class GetCoinInfoUseCase(
+    private val repository: CoinRepository,
+) {
+    operator fun invoke(fromSymbol: String): LiveData<CoinInfo> = repository.getCoinInfo(fromSymbol)
+}
