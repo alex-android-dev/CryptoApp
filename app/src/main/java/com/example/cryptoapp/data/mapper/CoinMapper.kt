08609160fe8593a60f49b88621c431252945ae23
avlnkov <alex.lenkov@mail.ru>
2025-02-01 19:49:32 +0300

    realized mapper json to List Coin Dto

--- app/src/main/java/com/example/cryptoapp/data/mapper/CoinMapper.kt
+++ app/src/main/java/com/example/cryptoapp/data/mapper/CoinMapper.kt
@@ -2,12 +2,13 @@ package com.example.cryptoapp.data.mapper
 
 import com.example.cryptoapp.data.database.CoinDbModel
 import com.example.cryptoapp.data.network.CoinDto
+import com.example.cryptoapp.data.network.CoinInfoJsonContainerDto
 import com.example.cryptoapp.data.network.CoinNameDto
+import com.google.gson.Gson
 
 class CoinMapper {
 
     // Dto -> DbModel
-
     fun dtoToDbModel(coinDto: CoinDto) = CoinDbModel(
         fromSymbol = coinDto.fromSymbol,
         toSymbol = coinDto.toSymbol,
@@ -19,6 +20,24 @@ class CoinMapper {
         imageUrl = coinDto.imageUrl,
     )
 
+    // json to List Coin Dto
+    fun jsonToListCoinDto(jsonContainer: CoinInfoJsonContainerDto): List<CoinDto> {
+        val result = mutableListOf<CoinDto>()
+        val jsonObject = jsonContainer.json ?: return result
+        val coinKeySet = jsonObject.keySet()
+        for (coinKey in coinKeySet) {
+            val currencyJson = jsonObject.getAsJsonObject(coinKey)
+            val currencyKeySet = currencyJson.keySet()
+            for (currencyKey in currencyKeySet) {
+                val priceInfo = Gson().fromJson(
+                    currencyJson.getAsJsonObject(currencyKey),
+                    CoinDto::class.java
+                )
+                result.add(priceInfo)
+            }
+        }
+        return result
+    }
 
 
 }
